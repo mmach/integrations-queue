61df2b69101f34c7dd55fcafe683fc4578b15317
mmach900 <m.mach.90@gmail.com>
2019-11-16 14:13:38 +0100

    no message

---
+++ index.js.js
@@ -0,0 +1,32 @@
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://kyqjanjv:6djuPiJWnpZnIMT1jZ-SvIULv8IOLw2P@hedgehog.rmq.cloudamqp.com/kyqjanjv', function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'integration-items';
+
+
+        channel.prefetch(1);
+
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        channel.consume(queue, function (msg) {
+            // throw msg;
+            console.log(msg.content.toString());
+            channel.ack(msg);
+
+            //  
+          
+
+            // console.log(" [x] Received %s", msg.content.toString());
+
+        }, {
+            noAck: false
+        });
+    });
+});
